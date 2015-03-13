9a41516fc0e3f83b2f285497c9f3f6d0cbaf67a6
unknown <britt336@B162-030.ad.mtroyal.ca>
2015-03-12 20:22:00 -0600

    added block rotations, fixed makefile, adjusted raster

--- Stage 3/Model/model.c
+++ Stage 3/Model/model.c
@@ -1,39 +1,217 @@
-#define GRID_WIDTH 10
-#define GRID_HEIGHT 18
+#include "model.h"
+/*
+0 = block
+1 = Zig Zag Right
+2 = Zig Zag Left
+3 = Straight
+4 = Corner Left
+5 = Corner Right
+6 = T-Block
+*/
+struct Shape shape;
 
-struct CellGrid {/*holds the array of grid cells containing the current state of the grid*/
-	Cell[10][18];
+const char blockR1[4][4] =
+{
+	1, 1, 0, 0,
+	1, 1, 0, 0,
+	0, 0, 0, 0,
+	0, 0, 0, 0
+};
+ 
+const char ZigZagRightR1[4][4] =
+{
+	0, 1, 1, 0,
+	1, 1, 0, 0,
+	0, 0, 0, 0,
+	0, 0, 0, 0
+};
+ 
+const char ZigZagRightR2[4][4] =
+{
+	1, 0, 0, 0,
+	1, 1, 0, 0,
+	0, 1, 0, 0,
+	0, 0, 0, 0
 };
 
-struct Cell { /*holds information about a single position in the tetris grid*/
-	bool isFull; /* has the cell been filled by the placement of a block or the presence of the current block*/
-	bool isOwned;/* is the cell generically filled or is it owned by the current shape*/
+const char ZigZagLeftR1[4][4] =
+{
+	1, 1, 0, 0,
+	0, 1, 1, 0,
+	0, 0, 0, 0,
+	0, 0, 0, 0
+};
+ 
+const char ZigZagLeftR2[4][4] =
+{
+	1, 1, 0, 0,
+	0, 1, 1, 0,
+	0, 0, 0, 0,
+	0, 0, 0, 0
+};
+ 
+const char StraightR1[4][4] =
+{
+	1, 0, 0, 0,
+	1, 0, 0, 0,
+	1, 0, 0, 0,
+	1, 0, 0, 0
+};
+ 
+const char StraightR2[4][4] =
+{
+	1, 1, 1, 1,
+	0, 0, 0, 0,
+	0, 0, 0, 0,
+	0, 0, 0, 0
+};
+ 
+const char CornerRightR1[4][4] =
+{
+	1, 0, 0, 0,
+	1, 0, 0, 0,
+	1, 1, 0, 0,
+	0, 0, 0, 0
 };
 
-struct Shape { /*holds the x and y positions of all cells pertaining to a single shape*/
-	int x1;
-	int y1;
-	int x2;
-	int y2;
-	int x3;
-	int y3;
-	int x4;
-	int y4;
+const char CornerRightR2[4][4] =
+{
+	1, 1, 1, 0,
+	1, 0, 0, 0,
+	0, 0, 0, 0,
+	0, 0, 0, 0
 };
 
-bool lowerShape(Shape s){ /*method lets us know if we can lower the shape by one cell*/
-	bool collision = false;
-	
-	int x = s->x1;
-	int y = s->y1;
+const char CornerRightR3[4][4] =
+{
+	1, 1, 0, 0,
+	0, 1, 0, 0,
+	0, 1, 0, 0,
+	0, 0, 0, 0
+};
+
+const char CornerRightR4[4][4] =
+{
+	0, 0, 1, 0,
+	1, 1, 1, 0,
+	0, 0, 0, 0,
+	0, 0, 0, 0
+};
+
+const char CornerLeftR1[4][4] =
+{
+	0, 1, 0, 0,
+	0, 1, 0, 0,
+	1, 1, 0, 0,
+	0, 0, 0, 0
+};
+
+const char CornerLeftR2[4][4] =
+{
+	1, 0, 0, 0,
+	1, 1, 1, 0,
+	0, 0, 0, 0,
+	0, 0, 0, 0
+};
+
+const char CornerLeftR3[4][4] =
+{
+	1, 1, 0, 0,
+	1, 0, 0, 0,
+	1, 0, 0, 0,
+	0, 0, 0, 0
+};
+
+const char CornerLeftR4[4][4] =
+{
+	1, 1, 1, 0,
+	0, 0, 1, 0,
+	0, 0, 0, 0,
+	0, 0, 0, 0
+};
+
+const char TblockR1[4][4] =
+{
+	0, 1, 0, 0,
+	1, 1, 1, 0,
+	0, 0, 0, 0,
+	0, 0, 0, 0
+};
+
+const char TblockR2[4][4] =
+{
+	0, 1, 0, 0,
+	0, 1, 1, 0,
+	0, 1, 0, 0,
+	0, 0, 0, 0
+};
+
+const char TblockR3[4][4] =
+{
+	0, 0, 0, 0,
+	1, 1, 1, 0,
+	0, 1, 0, 0,
+	0, 0, 0, 0
+};
+
+const char TblockR4[4][4] =
+{
+	0, 1, 0, 0,
+	1, 1, 0, 0,
+	0, 1, 0, 0,
+	0, 0, 0, 0
+};
+
+/* void move_ball(struct Ball *ball)
+{
+ball->x += ball->delta_x;
+ball->y += ball->delta_y;
+
+}
+*/
+
+/* int lowerShape(struct Shape s)
+{ 
+	int collision = 0;
 
 	return collision;
-}
+} */
 
-bool canLowerCell(int y){ /* method is a helper for lowerShape - tells us if a specific cell can move down one cell and still be in bounds*/
+int canLowerCell(int y) /* method is a helper for lowerShape - tells us if a specific cell can move down one cell and still be in bounds */
+{
 	int yTemp = y;
 	if (++yTemp == GRID_HEIGHT)
-		return false;
-	return true;
+		return 0;
+	return 1;
 }
 
+/*
+0 = block
+1 = Zig Zag Left
+2 = Zig Zag Right
+3 = Straight
+4 = Corner Right
+5 = Corner Left
+6 = T-Block
+*/
+
+void makeBlock (int blockNum)
+{
+	switch (blockNum)
+	{
+		case 0:
+			break;
+		case 1:
+			break;	
+		case 2:
+			break;	
+		case 3:
+			break;
+		case 4:
+			break;
+		case 5:
+			break;
+		case 6:
+			break;
+	}
+}
--- Stage 3/Model/model.h
+++ Stage 3/Model/model.h
@@ -1,2 +1,54 @@
-bool lowerShape(Shape s);
-bool canLowerCell(int y)
+#define GRID_WIDTH 10
+#define GRID_HEIGHT 18
+
+struct Cell  		/* holds information about a single position in the tetris grid */
+{
+	int isFull; 	/* has the cell been filled by the placement of a block or the presence of the current block */
+};
+
+struct Shape		/* 2d representation of a shape piece */
+{ 
+	int shapeArray[4][4];
+};
+
+struct Score
+{
+	int value;
+};
+
+struct Grid 	/* holds the array of grid cells containing the current state of the grid */
+{
+	struct Cell cells[GRID_WIDTH][GRID_HEIGHT];
+};
+
+struct Time
+{
+	int value;
+};
+
+struct Model
+{
+	struct Grid grid;
+	struct Score score;
+	struct Shape shape;
+	struct Time time;
+};
+
+/*
+0 = block
+1 = Zig Zag Right
+2 = Zig Zag Left
+3 = Straight
+4 = Corner Left
+5 = Corner Right
+6 = T-Block
+*/
+
+/* struct Block
+{
+ int *all_Block= {
+} */
+
+int lowerShape(struct Shape s);
+void makeBlock (int blockNum);
+int canLowerCell(int y);
